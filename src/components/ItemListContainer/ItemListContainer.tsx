76f1e566d3537a154fa4581fb9589ae6583071b2
Willian Silva <willian.silva.1992@hotmail.com>
2023-12-07 23:06:27 -0300

    ajustes

--- src/components/ItemListContainer/ItemListContainer.tsx
+++ src/components/ItemListContainer/ItemListContainer.tsx
@@ -2,35 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import ItemList from '../ItemList/ItemList';
 
-const mockData = [
-  {
-    id: 1,
-    title: 'Cerveja Heineken Lata 269ml',
-    description: 'Heineken é uma cerveja lager Puro Malte...',
-    price: 3.99,
-    pictureUrl: 'Heineken-269ml.jpg',
-    stock: 5, 
-  },
-];
 
 const ItemListContainer: React.FC = () => {
-  const handleDetailsClick = (description: string) => {
-    alert(`Detalhes do produto: ${description}`);
-  };
-
-  const [items, setItems] = useState(mockData);
-
-  useEffect(() => {
-    // Mock de busca de dados após 2 segundos
-    setTimeout(() => {
-      // fetchData();
-    }, 2000);
-  }, []);
-
   return (
     <div>
       <h2>Lista de Produtos</h2> <br></br>
-      <ItemList items={items} onDetailsClick={handleDetailsClick} />
+      <ItemList />
     </div>
   );
 };
